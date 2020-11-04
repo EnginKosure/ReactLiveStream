f611bd3fa949b9cba0ce9f1664736874aec43036
tostos <saittosun2@gmail.com>
2020-11-04 16:24:16 +0100

    navbar is added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {NavLink} from 'react-router-dom';
+import { Route } from 'react-router';
+
+import register from './containers/register/register';
+import profile from './containers/profile/profile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Aplaudo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <NavLink to="/" class="nav-link" href="#">Home <span class="sr-only">(current)</span></NavLink>
+            </li>
+            <li class="nav-item">
+              <NavLink to="/register" class="nav-link" href="#">Register</NavLink>
+            </li>
+            <li class="nav-item">
+              <NavLink to="/profile" class="nav-link" href="#">Profile</NavLink>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+      <Route path="/register" component={register}/>
+      <Route path="/profile" component={profile}/>
     </div>
   );
 }
---
+++ frontend/src/containers/register/register.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './register.css';
+
+class register extends Component {
+    render () {
+        return (
+            <div>
+                <h1>let us sign in!!!</h1>
+            </div>
+        );
+    }
+}
+
+export default register;
