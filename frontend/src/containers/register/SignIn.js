b53f4c22bed9c37cd04b88c1ab148bfb35ce57c0
EnginKosure <engin.kosure@cronos.be>
2020-11-10 00:34:30 +0100

    add register homepage

--- frontend/src/containers/register/SignIn.js
+++ frontend/src/containers/register/SignIn.js
@@ -14,12 +14,45 @@
 
 // export default register;
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { userActions } from '../../actions';
 import "./register.css";
 
 const Register = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  const [inputs, setInputs] = useState({
+    EmailAddress: '',
+    Password: ''
+  });
+  const [submitted, setSubmitted] = useState(false);
+  const { EmailAddress, Password } = inputs;
+  const loggingIn = useSelector(state => state.authentication.loggingIn);
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  // reset login status
+  useEffect(() => {
+    dispatch(userActions.logout());
+  }, []);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setInputs(inputs => ({ ...inputs, [name]: value }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setSubmitted(true);
+    if (EmailAddress && Password) {
+      // get return url from location state or default to home page
+      const { from } = location.state || { from: { pathname: "/" } };
+      dispatch(userActions.login(EmailAddress, Password, from));
+    }
+  }
 
   return (
     <div className="login-wrapper">
@@ -30,29 +63,42 @@ const Register = () => {
           alt="profile-img"
           className="profile-img-card photo"
         />
-        <form className="container">
+        <form className="container" name="form" onSubmit={handleSubmit}>
           <h3>Sign In</h3>
           <div className="form-group">
-            <label htmlFor="email">Email address</label>
+            <label htmlFor="EmailAddress">Email address</label>
             <input
               type="email"
-              name="email"
-              className="form-control email"
-              value={email}
+              name="EmailAddress"
+              className={'form-control email' + (submitted && !EmailAddress ? ' is-invalid' : '')}
+              value={EmailAddress}
               placeholder="Enter email"
-              onChange={e => setEmail(e.target.value)}
+              onChange={handleChange}
             />
+            {submitted && !EmailAddress &&
+              <div className="invalid-feedback">Email is required</div>
+            }
           </div>
           <div className="form-group">
-            <label htmlFor="password" >Password</label>
+            <label htmlFor="Password" >Password</label>
             <input
               type="password"
-              name="password"
-              className="form-control password"
-              value={password}
+              name="Password"
+              className={"form-control password" + (submitted && !Password ? " is-invalid" : "")}
+              value={Password}
               placeholder="Enter password"
-              onChange={e => setPassword(e.target.value)}
+              onChange={handleChange}
             />
+            {submitted && !Password &&
+              <div className="invalid-feedback">Password is required</div>
+            }
+          </div>
+          <div className="form-group">
+            <button className="btn btn-primary">
+              {loggingIn && <span className="spinner-border spinner-border-sm mr-1"></span>}
+                        Login
+                    </button>
+            <Link to="/signup" className="btn btn-link">Register</Link>
           </div>
           <button type="submit" className="btn btn-primary btn-block" > Submit</button >
           <p className="forgot-password text-right">
