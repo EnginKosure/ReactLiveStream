1abc03d9dfe688b93d72a94dba5e745620460e3d
EnginKosure <engin.kosure@cronos.be>
2020-11-09 16:05:53 +0100

    redesign sign-up module

--- frontend/src/containers/register/ArtistSignUp.js
+++ frontend/src/containers/register/ArtistSignUp.js
@@ -1,4 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { userActions } from '../../actions';
 import "./register.css";
 
 const ArtistSignUp = () => {
@@ -10,11 +14,42 @@ const ArtistSignUp = () => {
     //     "EmailAddress": "rass@gmail.com",
     //     "Password": "123dwccetere"
     // }
-    const [name, setName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [nickName, setNickName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    // const [name, setName] = useState("");
+    // const [lastName, setLastName] = useState("");
+    // const [nickName, setNickName] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [password, setPassword] = useState("");
+
+    const [user, setUser] = useState({
+        ArtistPhoto: '',
+        ArtistFirstName: '',
+        ArtistLastName: '',
+        ArtistNickName: '',
+        EmailAddress: '',
+        Password: ''
+    });
+    const [submitted, setSubmitted] = useState(false);
+    const registering = useSelector(state => state.registration.registering);
+    const dispatch = useDispatch();
+
+    // reset login status
+    useEffect(() => {
+        dispatch(userActions.logout());
+    }, []);
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setUser(user => ({ ...user, [name]: value }));
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        setSubmitted(true);
+        if (user.ArtistFirstName && user.ArtistLastName && user.EmailAddress && user.Password) {
+            dispatch(userActions.register(user));
+        }
+    }
 
     return (
         <div className="login-wrapper">
@@ -24,18 +59,21 @@ const ArtistSignUp = () => {
                     alt="profile-img"
                     className="profile-img-card photo"
                 />
-                <form className="container">
+                <form className="container" name="form" onSubmit={handleSubmit}>
                     <h3>Create an account</h3>
                     <div className="form-group">
                         <label htmlFor="ArtistFirstName">First name</label>
                         <input
                             type="text"
                             name="ArtistFirstName"
-                            className="form-control email"
-                            value={name}
+                            className={"form-control email" + (submitted && !user.ArtistFirstName ? ' is-invalid' : '')}
+                            value={user.ArtistFirstName}
                             placeholder="Enter first name"
-                            onChange={e => setName(e.target.value)}
+                            onChange={handleChange}
                         />
+                        {submitted && !user.ArtistFirstName &&
+                            <div className="invalid-feedback">First Name is required</div>
+                        }
                     </div>
 
                     <div className="form-group">
@@ -43,11 +81,14 @@ const ArtistSignUp = () => {
                         <input
                             type="text"
                             name="ArtistLastName"
-                            className="form-control email"
-                            value={lastName}
+                            className={"form-control email" + (submitted && !user.ArtistLastName ? ' is-invalid' : '')}
+                            value={user.ArtistLastName}
                             placeholder="Enter last name"
-                            onChange={e => setLastName(e.target.value)}
+                            onChange={handleChange}
                         />
+                        {submitted && !user.ArtistLastName &&
+                            <div className="invalid-feedback">Last Name is required</div>
+                        }
                     </div>
 
                     <div className="form-group">
@@ -56,9 +97,9 @@ const ArtistSignUp = () => {
                             type="text"
                             name="ArtistNickName"
                             className="form-control email"
-                            value={nickName}
+                            value={user.ArtistNickName}
                             placeholder="Enter nick name"
-                            onChange={e => setNickName(e.target.value)}
+                            onChange={handleChange}
                         />
                     </div>
 
@@ -67,24 +108,37 @@ const ArtistSignUp = () => {
                         <input
                             type="email"
                             name="EmailAddress"
-                            className="form-control email"
-                            value={email}
+                            className={"form-control email" + (submitted && !user.EmailAddress ? ' is-invalid' : '')}
+                            value={user.EmailAddress}
                             placeholder="Enter email"
-                            onChange={e => setEmail(e.target.value)}
+                            onChange={handleChange}
                         />
+                        {submitted && !user.EmailAddress &&
+                            <div className="invalid-feedback">Email is required</div>
+                        }
                     </div>
                     <div className="form-group">
                         <label htmlFor="Password" >Password</label>
                         <input
                             type="password"
                             name="Password"
-                            className="form-control password"
-                            value={password}
+                            className={"form-control password" + (submitted && !user.Password ? ' is-invalid' : '')}
+                            value={user.Password}
                             placeholder="Enter password"
-                            onChange={e => setPassword(e.target.value)}
+                            onChange={handleChange}
                         />
+                        {submitted && !user.Password &&
+                            <div className="invalid-feedback">Password is required</div>
+                        }
                     </div>
                     <button type="submit" className="btn btn-primary btn-block" > Submit</button >
+                    <div className="form-group">
+                        <button className="btn btn-primary">
+                            {registering && <span className="spinner-border spinner-border-sm mr-1"></span>}
+                        Register
+                    </button>
+                        <Link to="/signin" className="btn btn-link">Cancel</Link>
+                    </div>
                 </form>
             </div>
         </div>
