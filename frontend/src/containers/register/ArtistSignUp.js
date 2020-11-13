625b334f89cae66a4b20743498fd6abb96a2b5ed
EnginKosure <engin.kosure@cronos.be>
2020-11-13 15:05:39 +0100

    apply design to sign in component

--- frontend/src/containers/register/ArtistSignUp.js
+++ frontend/src/containers/register/ArtistSignUp.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { userActions } from '../../actions';
-import "./register.css";
+import "./register.scss";
 import aplaudo from '../../assets/aplaudo.png'
 
 
