ddfedebdebd4e44589bb30820044f57e856a31ac
tostos <saittosun2@gmail.com>
2020-11-17 23:37:24 +0100

    imdb api is added

--- frontend/src/containers/Carousel/Item/Item.js
+++ frontend/src/containers/Carousel/Item/Item.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
 import styled from "styled-components";
 import "./Item.scss";
 import { Link } from 'react-router-dom';
@@ -17,7 +18,35 @@ const StyledDiv = styled.div`
   cursor: pointer; 
 `;
 
+
+
 const Item = () => {
+  const [concerts, setConcerts] = useState({})
+
+  const options = {
+    method: 'GET',
+    url: 'https://robby.p.rapidapi.com/search.json',
+    params: {
+      country: 'US',
+      lng: '-74.00597',
+      city: 'New York',
+      lat: '40.71435',
+      to: '2020-08-01T20:30:00+08:00',
+      limit: '30',
+      distance: '10',
+      from: '2019-06-30T20:30:00'
+    },
+    headers: {
+      'x-rapidapi-key': '15ad104108mshf6f08f42562fee9p141a21jsne13a7acdf194',
+      'x-rapidapi-host': 'robby.p.rapidapi.com'
+    }
+  };
+
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
  return (
   <StyledDiv>
     <Link to="/concertInfo">
