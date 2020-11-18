15da827db5d6ca51d2102dd6080a1499203191ba
EnginKosure <engin.kosure@cronos.be>
2020-11-18 09:21:00 +0100

    checkout main

--- frontend/src/containers/Concert/ConcertForm.js
+++ frontend/src/containers/Concert/ConcertForm.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import CheckBoxGenre from './CheckBoxGenre';
+import Button from 'react-bootstrap/Button';
+// import Form from 'react-bootstrap/Form';
+import { Grid, Row, Col, Form } from 'react-bootstrap';
+
 
 // import { concertActions } from '../../actions';
 import "./concert.scss";
@@ -99,6 +103,59 @@ const ConcertForm = () => {
                     <div className="main-profile-update">
 
                         <div className="left-form">
+
+
+                            <Form>
+                                <Form.Row>
+                                    <Form.Group as={Col} controlId="formGridEmail">
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control type="email" placeholder="Enter email" />
+                                    </Form.Group>
+
+                                    <Form.Group as={Col} controlId="formGridPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" placeholder="Password" />
+                                    </Form.Group>
+                                </Form.Row>
+
+                                <Form.Group controlId="formGridAddress1">
+                                    <Form.Label>Address</Form.Label>
+                                    <Form.Control placeholder="1234 Main St" />
+                                </Form.Group>
+
+                                <Form.Group controlId="formGridAddress2">
+                                    <Form.Label>Address 2</Form.Label>
+                                    <Form.Control placeholder="Apartment, studio, or floor" />
+                                </Form.Group>
+
+                                <Form.Row>
+                                    <Form.Group as={Col} controlId="formGridCity">
+                                        <Form.Label>City</Form.Label>
+                                        <Form.Control />
+                                    </Form.Group>
+
+                                    <Form.Group as={Col} controlId="formGridState">
+                                        <Form.Label>State</Form.Label>
+                                        <Form.Control as="select" defaultValue="Choose...">
+                                            <option>Choose...</option>
+                                            <option>...</option>
+                                        </Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group as={Col} controlId="formGridZip">
+                                        <Form.Label>Zip</Form.Label>
+                                        <Form.Control />
+                                    </Form.Group>
+                                </Form.Row>
+
+                                <Form.Group id="formGridCheckbox">
+                                    <Form.Check type="checkbox" label="Check me out" />
+                                </Form.Group>
+
+                                <Button variant="primary" type="submit">
+                                    Submit
+  </Button>
+                            </Form>
 
                             <div className="form-group">
                                 <label htmlFor="About" />
