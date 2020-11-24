485bcc9bf8068aa213062aeffa0d46d44f6ff4c5
EnginKosure <engin.kosure@cronos.be>
2020-11-24 12:16:43 +0100

    update ApiService

--- frontend/src/helpers/ApiService.js
+++ frontend/src/helpers/ApiService.js
@@ -1,7 +1,7 @@
-// Wouter Wouters
-//TODO: For now, we use the full paths in api calls, later it will be automated via this module.
-const baseURLCloud = window && window._env_
-    ? window._env_.REACT_APP_CLOUD_URL
+import axios from 'axios'
+
+const baseURLCloud = window && window.env
+    ? window.env.REACT_APP_CLOUD_URL
     : "http://localhost:8080";
 
 
@@ -24,23 +24,20 @@ export default class ApiService {
     }
     async get(url, config) {
         try {
-            const result = awai
-            Wouter Wouters10: 30
-            async get(url, config) {
-                try {
-                    const result = await this.api.get(url, config);
-                    return result;
-                } catch (error) {
-                    handleError(error);
-                }
-            }
+            const result = await this.api.get(url, config);
+            return result;
+        } catch (error) {
+            // handleError(error);
+        }
+    }
 
-            async post(url, data, config) {
-                try {
-                    console.log(data);
-                    const result = await this.api.post(url, data, config);
-                    return result;
-                } catch (error) {
-                    handleError(error);
-                }
-            }
+    async post(url, data, config) {
+        try {
+            console.log(data);
+            const result = await this.api.post(url, data, config);
+            return result;
+        } catch (error) {
+            // handleError(error);
+        }
+    }
+}
