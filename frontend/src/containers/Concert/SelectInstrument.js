8122974c15847d90b9f1da5eaaf0904732556fc6
EnginKosure <engin.kosure@cronos.be>
2020-11-30 22:49:48 +0100

    remove unused code

--- frontend/src/containers/Concert/SelectInstrument.js
+++ frontend/src/containers/Concert/SelectInstrument.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import MultiSelect from "react-multi-select-component";
 import "./SelectedInstrument.scss"
-
 
 function SelectInstrument({ onChange, value, name }) {
     const options = [
